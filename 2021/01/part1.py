3803ce168147067de384a28bbe036bc13ec7e161
Jan Steeg <jsteeg@posteo.de>
2021-12-03 14:02:17 +0100

    update

--- 2021/01/part1.py
+++ 2021/01/part1.py
@@ -1,12 +1,10 @@
 from utilities import load_data_as_list
 
-DATA = "data.txt"
 
-
-def puzzle():
-    content = load_data_as_list(DATA, int)
+def puzzle(data_file: str) -> int:
+    content = load_data_as_list(data_file, int)
     return len([1 for i in range(1, len(content) - 1) if content[i] > content[i-1]])
 
 
 if __name__ == "__main__":
-    print(puzzle())
+    print(puzzle("data.txt"))
--- 2021/01/part2.py
+++ 2021/01/part2.py
@@ -1,12 +1,10 @@
 from utilities import load_data_as_list
 
-DATA = "data.txt"
 
-
-def puzzle() -> int:
-    content = load_data_as_list(DATA, int)
+def puzzle(data_file: str) -> int:
+    content = load_data_as_list(data_file, int)
     return len([1 for i in range(2, len(content) - 1) if sum(content[i - 2:i + 1]) > sum(content[i - 3:i])])
 
 
 if __name__ == "__main__":
-    print(puzzle())
+    print(puzzle("data.txt"))
