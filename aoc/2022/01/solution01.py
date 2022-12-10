7d11200ea76ac5015486608a43b5957e36daf637
Jan Steeg <jan.steeg@inovex.de>
2022-12-10 16:12:38 +0100

    day 01 update

--- aoc/2022/01/solution01.py
+++ aoc/2022/01/solution01.py
@@ -11,7 +11,7 @@ def day01():
     data = ((int(y) for y in x.split()) for x in load_data_as_list(DIR.joinpath("input.txt"), split="\n\n"))
     each = sorted(map(sum, data), reverse=True)
     print(f"Top: {each[0]}")
-    print(f"Sum of top: {sum(each[0:3])}")
+    print(f"Sum of top 3: {sum(each[0:3])}")
 
 
 if __name__ == "__main__":
